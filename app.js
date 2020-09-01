f98bc5e6bd8c3c77bf0206b34e9ba5c34cd03659
Maddy Murguia <maddymurguia@Maddys-Air.hsd1.ca.comcast.net>
2020-08-31 18:33:01 -0700

    succesfully pulled properties per userInput

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 // console.log('testing 1..2..3')
 //console log tested and working 
-const $name  = $('#name');
-const $userInput = $('').val();
-// Inside of this ajax request / add + userInput 
-
 
 $(() => {
-    $.ajax(
-        {
-            url:'https://pokeapi.co/api/v2/pokemon'
-        }
-    ).then(
-        (data) => {
-            console.log(data)
-        }
-    )
+        const $name  = $('#name');
+        $('#submitBtn').on('click', (event) => {
+            event.preventDefault();
+            const $userInput = $('#inputType').val();
+        // Inside of this ajax request / add + userInput 
+        console.log($userInput);
+        $.ajax(
+            {
+                url:'https://pokeapi.co/api/v2/pokemon/' + $userInput
+            }
+        ).then(
+            (data) => {
+                console.log(data)
+            }
+        )
+    })
 })
+//append data to modal div
 
 
 //succesfully pulled data /array of pokemon 
