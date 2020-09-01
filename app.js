3a8f7df709ea719921dde06db711f6581ca9205c
Maddy Murguia <maddymurguia@Maddys-Air.hsd1.ca.comcast.net>
2020-08-31 18:10:50 -0700

    added search bar and submit btn

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
 // console.log('testing 1..2..3')
 //console log tested and working 
+const $name  = $('#name');
+const $userInput = $('').val();
+// Inside of this ajax request / add + userInput 
+
+
+$(() => {
+    $.ajax(
+        {
+            url:'https://pokeapi.co/api/v2/pokemon'
+        }
+    ).then(
+        (data) => {
+            console.log(data)
+        }
+    )
+})
+
+
+//succesfully pulled data /array of pokemon 
+ 
+//attempting to call on individual pokemon from array [0] to determine modal names such as height name etc..
+
+
+//append data.count to the body 
+
